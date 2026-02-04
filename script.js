1fc2cb6170906f800a063b720e6e26671d9951cc
chiomavera <you@example.com>
2026-02-04 18:03:33 +0100

    Page revamp rework landing page for clarity, balance, and visual cohesion

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-let mainNav = document.getElementById('js-menu');
+// ====== Nagvition ==============
+const toggle = document.querySelector(".nav-toggle");
+const menu = document.querySelector("#nav-menu");
 
-let navBarToggle = document.getElementById('js-Navbar-toggle');
+toggle.addEventListener("click", () => {
+  const isOpen = menu.classList.toggle("open");
+  toggle.setAttribute("aria-expanded", isOpen);
+});
 
-navBarToggle.addEventListener('click', function(){
-    mainNav.classList.toggle('active');
-})
+// ====== Booking System ==============
+// In-memory storage of bookings
+const bookings = [];
+
+const bookingForm = document.querySelector("#booking-form");
+const bookingCofirmation = document.querySelector("#booking-confirmation");
+
+bookingForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+// collect the values
+const clientName = bookingForm.name.value;
+const email = bookingForm.email.value;
+const service = bookingForm.service.value;
+const packageType = bookingForm.package.value;
+const date = bookingForm.date.value;
+const time = bookingForm.time.value;
+
+// Save booking
+bookings.push({clientName, email, service, packageType, date, time })
+console.log(bookings);
+
+bookingForm.reset();
+bookingCofirmation.classList.remove("hidden");
+});
