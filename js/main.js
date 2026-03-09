a585eb88acb7034165f7a4cf6a36f6ce10ea662b
chiomavera <you@example.com>
2026-03-10 00:11:32 +0100

    refactor: update booking import path and remove unused supabase client

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-import { initBooking } from "./booking.js";
+import { initBooking } from "./booking/index.js";
 import { initNavigation } from "./navigation.js";
-
-const supabase = createClient(
-  "https://llmwcqivyuqytbtqcaoz.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsbXdjcWl2eXVxeXRidHFjYW96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMjE0NzMsImV4cCI6MjA4NTY5NzQ3M30.4UBQ8aPfdTylIyjWUjr7rn2xvcRmpizd1elxxmjGMVk",
-);
 
 initNavigation();
 initBooking();
